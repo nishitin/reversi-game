52bdf22d6a9120e04a78ced15023a3e8816d08c1
nisitin <seiya110307@icloud.com>
2021-11-15 17:22:56 +0900

    fix: 修正

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,29 +1,27 @@
 import { useState } from "react"
 
 export const Board = () => {
-//先行と後攻をstateで管理する //trueの場合白が先行
-  const [player, setPlayer] = useState(true)
+  // const whitePlayer = setPlayer(true)
+  // const blackPlayer = setPlayer(false)
 
+  // console.log(blackPlayer)
 
   //メモ: 白が値１ 黒が値2 何も置いてない場所は0にしてその総数で勝敗を決める
   //メモ: 1同士に挟まれたものはひっくり返す
   //最初におけるのはwhiteプレイヤーが先行の場合 x:4, y:2 x:5, y:3
   // white [2][4], [3][5] [4][2] [5][2]
   // black [2][3], [3][2] [4][5], [5][4]
-  const board = [
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,1,2,0,0,0],
-    [0,0,0,2,1,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0]
-  ]
-  console.log(board.length)
+  const boardNumber = new Array(8)
+  const turn = 0 //初期値0　1 黒先行 -1白先行
+
+
+    for (let i = 0; i < boardNumber.length; i++){
+      boardNumber[i] = new Array(8)
+    // 8 * 8が完成
+  }
+
 
   return (
-
     <div>
       <table border="1">
         <tr>

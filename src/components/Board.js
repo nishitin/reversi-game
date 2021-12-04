165f5fa5a990cd70da20c0576993526f46995e1b
nisitin <seiya110307@icloud.com>
2021-12-04 16:40:05 +0900

    数字判定

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,100 +11,40 @@ export const Board = () => {
   //最初におけるのはwhiteプレイヤーが先行の場合 x:4, y:2 x:5, y:3
   // white [2][4], [3][5] [4][2] [5][2]
   // black [2][3], [3][2] [4][5], [5][4]
-  const boardNumber = new Array(8)
-  const turn = 0 //初期値0　1 黒先行 -1白先行
+  let boardArr = new Array(8)
 
-
-    for (let i = 0; i < boardNumber.length; i++){
-      boardNumber[i] = new Array(8)
-    // 8 * 8が完成
+  for (let i = 0; i < 8; i++){
+    boardArr[i] = new Array(8)
+    for ( let j = 0; j < 8; j++){
+      boardArr[i][j] = 0
+    }
   }
-
+  //メモ　ひとつで渡したい　要チェックや
+  boardArr[4][4] = 1
+  boardArr[3][3] = 1
+  boardArr[3][4] = -1
+  boardArr[4][3] = -1
+  console.table(boardArr)
 
   return (
     <div>
+      <p className="board-text">アイウエオ</p>
       <table border="1">
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>●</th>
-          <th>◯</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>◯</th>
-          <th>●</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
+          {boardArr.map ((e) => {
+            return (
+             <tr className="table-tr">
+               <th>{e[1]}</th>
+               <th>{e[2]}</th>
+               <th>{e[3]}</th>
+               <th>{e[4]}</th>
+               <th>{e[5]}</th>
+               <th>{e[6]}</th>
+               <th>{e[7]}</th>
+             </tr>
+            )
+          })}
       </table>
+      <button>ゲームスタート</button>
     </div>
   )
 }
